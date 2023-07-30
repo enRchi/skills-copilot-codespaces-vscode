b52343f69c498fb8147c5e1358dfe91ccfa8f12f
Roger G <enRchi@users.noreply.github.com>
2023-07-30 10:17:59 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,57 +1,12 @@
 // create web Server
-var express = require('express');
-var app = express();
-var fs = require('fs');
-var bodyParser = require('body-parser');
-var multer = require('multer');
-var upload = multer({ dest: 'uploads/' });
-var path = require('path');
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-// connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/Comments');
-
-// create model
-var commentSchema = new Schema({
-    name: String,
-    comment: String,
-    date: { type: Date, default: Date.now }
+// 1. create http module
+const http = require('http');
+// 2. create web server
+const server = http.createServer((req, res) => {
+    // 3. send response
+    res.write('Hello World');
+    res.end();
 });
-
-var Comment = mongoose.model('Comment', commentSchema);
-
-// use body-parser
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// set view engine
-app.set('views', './views');
-app.set('view engine', 'pug');
-
-// set public directory
-app.use(express.static('public'));
-
-// set router
-app.get('/comments/new', function (req, res) {
-    res.render('new');
-});
-
-app.get('/comments', function (req, res) {
-    Comment.find({}, function (err, comments) {
-        if (err) return res.json(err);
-        res.render('index', { comments: comments });
-    });
-});
-
-app.post('/comments', function (req, res) {
-    Comment.create(req.body, function (err, comment) {
-        if (err) return res.json(err);
-        res.redirect('/comments');
-    });
-});
-
-// set server port
-app.listen(3000, function () {
-    console.log('Server On!');
-});
+// 4. listen on port 3000
+server.listen(3000);
+console.log('Listening on port 3000...');
