8331c7fdc6225affdd042fbdc85f6522dd5c28dc
Roger G <enRchi@users.noreply.github.com>
2023-07-30 09:58:49 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,24 @@
+// Create Web Server
+// Create DB Server
+// Create API Server
+// Create API
+// Create API Router
+// Create API Controller
+// Create API Model
+// Create API View
+
+const express = require('express');
+const router = express.Router();
+
+const commentController = require('../controllers/comments');
+
+// Create
+router.post('/', commentController.create);
+// Read
+router.get('/:id', commentController.read);
+// Update
+router.put('/:id', commentController.update);
+// Delete
+router.delete('/:id', commentController.delete);
+
+module.exports = router;
