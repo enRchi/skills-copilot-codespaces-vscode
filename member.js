f11526265c1e14ba05c040ba357550090e42a658
Roger G <enRchi@users.noreply.github.com>
2023-07-30 09:55:06 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'React', 'Node'];
+    var member = {
+        name: 'John Doe',
+        age: 34,
+        skills: skills
+    };
+    return member;
+}
